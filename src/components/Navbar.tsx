b975a081ad76be71de3136fa07eb27310f1c7c5a
Ahmed safwat <a01273477051@gmail.com>
2024-03-05 01:07:14 +0200

    added the main assets and the configuration of svg imgs

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,30 @@
-import { Link } from "react-router-dom"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import Logo from "../assets/logo.svg";
 
+export const Navbar = () => {
+  return (
+    <header className="p-10 flex justify-between items-center">
+      <Link to={"/"}>
+        <img src={Logo} alt="Logo"></img>
+      </Link>
+      <select>
+        <option>Shop</option>
+        <option>Hoodie</option>
+        <option>jeans</option>
+      </select>
+      <a>New Arrivals</a>
+      <a>Brands</a>
+      <input
+        type="search"
+        placeholder="Search For Product..."
+        className="bg-whitedark w-80 p-2 rounded-lg pl-6"
+      />
 
-export const Navbar = ()=>{
-    return(
-        
-        <header className="p-10 flex justify-between items-center">
-            <Link to={"/"} className="text-2xl font-bold ">SHOP.CO </Link>
-            <select>
-                <option>Shop</option>
-                <option>Hoodie</option>
-                <option>jeans</option>
-            </select>
-            <a>New Arrivals</a>
-            <a>Brands</a>
-            <input type="search" placeholder="Search For Product..." 
-            className="bg-whitedark w-80 p-2 rounded-lg pl-6"/>
-            
-            <Link to={"/Cart"}><FontAwesomeIcon icon={faCartShopping} className="text-xl"/></Link>
-            
-        </header>
-        )
-    
-} 
+      <Link to={"/Cart"}>
+        <FontAwesomeIcon icon={faCartShopping} className="text-xl" />
+      </Link>
+    </header>
+  );
+};
