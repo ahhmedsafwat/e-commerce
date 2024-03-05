45a0cb3bd66bfb552fe6453b41fc2c66a8ecc2a6
Ahmed safwat <a01273477051@gmail.com>
2024-03-06 01:58:11 +0200

    slight diffrence

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,9 +3,11 @@ import Navitems from "./Navitems";
 
 const Navbar = () => {
   return (
-    <header className="px-24 py-6 flex justify-between items-center">
-      <Navitems />
-      <Links />
+    <header className="px-24 py-6 flex justify-between items-center ">
+      <div className="container">
+        <Navitems />
+        <Links />
+      </div>
     </header>
   );
 };
