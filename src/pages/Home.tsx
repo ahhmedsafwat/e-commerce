5b855d12d20b98c1058fc9ef5d18b4582d1dda0a
elsnoty <mostafashawky399@gmail.com>
2024-03-09 23:30:25 +0200

    hreo

---
+++ src/pages/Home.tsx
@@ -0,0 +1,24 @@
+
+const Hero = ()=>{
+    return(
+        <div className="bg-whitedark flex">
+            <div className="">
+                <h1 className="font-satoshiBold text-6xl">
+                    FIND CLOTHES THAT MATCHES YOUR STYLE
+                </h1>
+                <p className="font-satoshireguler min-w-full">Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality and cater to your sense of style.</p>
+                <button>Shop Now</button>
+                <div>
+                    <h1>200+ <span className="block">International Brands</span></h1>
+                    <h1>2000+ <span className="block">High-Quality Products</span></h1>
+                    <h1>30,000+ <span className="block">Happy Customers</span></h1>
+                </div>
+                
+            </div>
+
+            
+        </div>
+    )
+}
+
+export default Hero;
