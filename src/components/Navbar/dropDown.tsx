e7a124e6ba6924c82b878e4e406537428310252c
elsnoty <mostafashawky399@gmail.com>
2024-03-09 21:41:09 +0200

    edit

--- src/components/Navbar/dropDown.tsx
+++ src/components/Navbar/dropDown.tsx
@@ -1,39 +1,46 @@
-import { useState } from "react";
-import { ChevronUp, ChevronDown } from "lucide-react";
+import { useState, useEffect, useRef } from "react";
+import { ChevronUp, ChevronDown } from 'lucide-react';
 
 const DropValue = () => {
-  const [open, setOpen] = useState<boolean>(false);
+    const [open, setOpen] = useState<boolean>(false);
+    const dropdownRef = useRef<HTMLDivElement>(null);
 
-  const handleOpen = () => {
-    setOpen(!open);
-  };
+    useEffect(() => {
+        const handleOutsideClick = (event: MouseEvent) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+                setOpen(false);
+            }
+        };
 
-  return (
-    <div className="relative">
-      <div className="flex items-center cursor-pointer" onClick={handleOpen}>
-        Shop {open ? <ChevronUp /> : <ChevronDown />}
-      </div>
+        window.addEventListener("click", handleOutsideClick);
 
-      <ul
-        className={`mt-2 border border-gray-300 rounded-md shadow-md bg-white absolute w-40 left-[-10px] ${
-          open ? "scale-0" : "scale-1"
-        } transition  origin-top`}
-      >
-        <li className="py-2 px-4 cursor-pointer hover:bg-gray-300 transition-colors">
-          Casual
-        </li>
-        <li className="py-2 px-4 cursor-pointer hover:bg-gray-300 transition-colors">
-          Hoodie
-        </li>
-        <li className="py-2 px-4 cursor-pointer hover:bg-gray-300 transition-colors">
-          Jeans
-        </li>
-        <li className="py-2 px-4 cursor-pointer hover:bg-gray-300 transition-colors">
-          Shoes
-        </li>
-      </ul>
-    </div>
-  );
-};
+        return () => {
+            window.removeEventListener("click", handleOutsideClick);
+        };
+    }, []);
+
+    const handleOpen = () => {
+        setOpen(!open);
+    }
+
+    return (
+        <div className="relative" ref={dropdownRef}>
+            <div className="flex items-center cursor-pointer" onClick={handleOpen}>
+                Shop {open ? <ChevronUp /> : <ChevronDown /> }
+            </div>
+
+            {open &&
+                <ul className="mt-2 border border-gray-300 rounded-md shadow-md bg-white absolute duration-300 delay-[50] transition-all hover:bg-gray-300 ">
+                    <li className="py-2 px-4 cursor-pointer hover:bg-gray-100">Casual</li>
+                    <li className="py-2 px-4 cursor-pointer hover:bg-gray-100">Hoodie</li>
+                    <li className="py-2 px-4 cursor-pointer hover:bg-gray-100">Jeans</li>
+                    <li className="py-2 px-4 cursor-pointer hover:bg-gray-100">Shoes</li>
+                </ul>
+            }
+
+        </div>
+    )
+}
 
 export default DropValue;
+
