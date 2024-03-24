6a2626e9763bd8034b82d02469382bd41ce687e2
Ahmed safwat <a01273477051@gmail.com>
2024-03-24 22:42:14 +0200

    hot fix #1

--- src/pages/Topselling/Topselling.tsx
+++ src/pages/Topselling/Topselling.tsx
@@ -8,21 +8,23 @@ const Topselling = () => {
     "topseller"
   );
   if (error) return `There was an error : ${error.message}`;
-  if (isPending)
-    return (
-      <div className="flex justify-between items-center py-16 xLarge:px-6 px-20 medium:px-8 medium:pt-28 small:pt-40">
-        <ProductSkeleton />
-        <ProductSkeleton />
-        <ProductSkeleton />
-        <ProductSkeleton />
-      </div>
-    );
+
   return (
     <div className=" py-16 px-20 xLarge:px-5 medium:px-8">
       <h1 className="text-center text-5xl font-secondary">Top Selling</h1>
-      <div className="grid grid-cols-4 large:grid-cols-2 gap-[1%] small:flex mt-14 mb-8 overflow-hidden place-items-center ">
-        <ProductItems products={fakeData} />
-      </div>
+      {isPending && (
+        <div className="flex justify-between items-center py-16 xLarge:px-6 px-20 medium:px-8 medium:pt-28 small:pt-40">
+          <ProductSkeleton />
+          <ProductSkeleton />
+          <ProductSkeleton />
+          <ProductSkeleton />
+        </div>
+      )}
+      {fakeData && (
+        <div className="grid grid-cols-4 large:grid-cols-2 gap-[1%] small:flex mt-14 mb-8 overflow-hidden place-items-center ">
+          <ProductItems products={fakeData} />
+        </div>
+      )}
       <Link
         to={"/category page"}
         className="block w-fit small:w-full text-center mx-auto mt-6 py-4 px-20 small:py-2 border rounded-[62px] font-satoshi500 hover:text-white hover:bg-black hover:shadow-lg  transition-colors duration-300"
