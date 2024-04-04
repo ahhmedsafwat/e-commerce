ee5f57457a7101dbb488b61eb9591e3bb6d0e17d
Ahmed safwat <a01273477051@gmail.com>
2024-04-04 05:23:05 +0200

    edited the hero sections font

--- src/pages/Hero/right.tsx
+++ src/pages/Hero/right.tsx
@@ -4,10 +4,10 @@ import Statistics from "./statistics";
 const Right = () => {
   return (
     <div className="w-[45%] large:w-full large:text-center medium:text-left pb-20 pt-10 large:pb-0 large:pt-0">
-      <h1 className="font-secondary text-[55px] leading-[64px] xLarge:text-5xl large:text-6xl medium:text-5xl pb-7 small:text-[34px]">
+      <h1 className="font-secondary text-[55px] leading-[64px] xLarge:text-5xl large:text-6xl medium:text-5xl pb-7 small:text-[34px] bg-clip-text text-transparent bg-gradient-to-r from-[#29323C] to-blue-600">
         FIND CLOTHES THAT MATCHES YOUR STYLE
       </h1>
-      <p className="font-satoshireguler mb-7  text-gray-500 ">
+      <p className="font-satoshireguler mb-7 text-gray-500">
         Browse through our diverse range of meticulously crafted garments,
         designed to bring out your individuality and cater to your sense of
         style.
