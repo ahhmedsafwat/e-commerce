1484d5568699e57d853371ea55e9f791d6e6226b
Ahmed safwat <a01273477051@gmail.com>
2024-04-08 02:42:54 +0200

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: { port: 3000 },
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    server: { port: 3000 },
+    base: "/",
+  };
+  if (command !== "serve") {
+    config.base = "/e-commerce";
+  }
+
+  return config;
 });
