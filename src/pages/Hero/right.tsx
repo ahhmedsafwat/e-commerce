98876d8bd0926409966a5fe68082b5750e193808
Ahmed safwat <a01273477051@gmail.com>
2024-04-08 05:51:39 +0200

    done with the useTypingEffect hook

--- src/pages/Hero/right.tsx
+++ src/pages/Hero/right.tsx
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
 import Button from "../../components/utilities/button";
 import Statistics from "./statistics";
+import { useTypeingEffect } from "../../components/utilities/useTypingEffect";
+
 const Right = () => {
+  const text = "FIND CLOTHES THAT MATCHES YOUR STYLE";
+  const textToshow = useTypeingEffect(text, 110, true);
+
   return (
     <div className="w-[45%] large:w-full large:text-center medium:text-left pb-20 pt-10 large:pb-0 large:pt-0">
-      <h1 className="font-secondary text-[55px] leading-[64px] xLarge:text-5xl large:text-6xl medium:text-5xl pb-7 small:text-[34px] bg-clip-text text-transparent bg-gradient-to-r from-[#29323C] to-blue-600">
-        FIND CLOTHES THAT MATCHES YOUR STYLE
+      <h1 className="h-[220px] font-secondary text-[55px] leading-[64px]  xLarge:text-5xl large:text-6xl medium:text-5xl pb-7 small:text-[34px] bg-clip-text text-transparent bg-gradient-to-r from-[#29323C] to-blue-600">
+        {textToshow}
       </h1>
       <p className="font-satoshireguler mb-7 text-gray-500">
         Browse through our diverse range of meticulously crafted garments,
